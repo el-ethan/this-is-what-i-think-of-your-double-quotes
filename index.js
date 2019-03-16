88f305fb4e40bc96761bd946439e96a1c907d4a5
el-ethan <elethan@gmail.com>
2019-03-16 13:47:05 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const program = require('commander');
+
+function main() {
+    program
+        .arguments('<fileToLint>')
+        .action((fileToLint) => fixDoubleQuotes(fileToLint))
+        .parse(process.argv);
+
+}
+
+function fixDoubleQuotes(file) {
+    const fileContents = fs.readFileSync(file, 'utf8');
+    const writeStream = fs.createWriteStream(file);
+    const updatedFileContents = fileContents.replace(/"/g, '💩');
+    console.log(updatedFileContents);
+    writeStream.write(updatedFileContents);
+    writeStream.end();
+}
+
+main();
